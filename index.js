7c88f665bde67189fdc41aa7387d1004ba3a8e06
@All-that-code <amaia.arbizu.mail@gmail.com>
2024-01-29 17:08:37 +0100

    Injected new weather icons to JS and restyled CSS after it

--- index.js
+++ index.js
@@ -6,13 +6,55 @@ function updateTemperature(response) {
   let timeElement = document.querySelector("#time");
   let currentHumidity = response.data.temperature.humidity;
   let humidityElement = document.querySelector("#humidity");
-  let date = new Date(response.data.time * 1000);
-
   let currentWind = response.data.wind.speed;
   let formattedWind = Math.round(currentWind);
   let windElement = document.querySelector("#wind");
   let currentCondition = response.data.condition.description;
   let conditionElement = document.querySelector("#sky");
+  let date = new Date(response.data.time * 1000);
+
+  let weatherCondition = response.data.condition.description;
+  let iconUrl = response.data.condition.icon_url;
+
+  console.log("Weather Condition:", weatherCondition);
+
+  if (weatherCondition === "clear sky") {
+    iconUrl =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/112/614/original/sun.png?1706464603";
+  } else if (weatherCondition === "few clouds") {
+    iconUrl =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/112/624/original/cloud.png?1706464787";
+  } else if (weatherCondition === "scattered clouds") {
+    iconUrl =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/112/624/original/cloud.png?1706464787";
+  } else if (weatherCondition === "broken clouds") {
+    iconUrl =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/112/618/original/clouded.png?1706464645";
+  } else if (weatherCondition === "shower rain") {
+    iconUrl =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/112/622/original/strong_rain.png?1706464692";
+  } else if (weatherCondition === "rain") {
+    iconUrl =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/112/619/original/light_rain.png?1706464654";
+  } else if (weatherCondition === "thunderstorm") {
+    iconUrl =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/112/623/original/thunderstorm.png?1706464700";
+  } else if (weatherCondition === "light snow") {
+    iconUrl =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/112/621/original/snow.png?1706464669";
+  } else if (weatherCondition === "mist") {
+    iconUrl =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/112/620/original/mist.png?1706464661";
+  } else if (weatherCondition === "overcast clouds") {
+    iconUrl =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/112/618/original/clouded.png?1706464645";
+  } else if (weatherCondition === "light rain") {
+    iconUrl =
+      "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/112/619/original/light_rain.png?1706464654";
+  }
+
+  let weatherIcon = document.querySelector("#icon");
+  weatherIcon.innerHTML = `<img src="${iconUrl}"/>`;
 
   humidityElement.innerHTML = `${currentHumidity}%`;
   windElement.innerHTML = `${formattedWind}km/h`;
@@ -21,6 +63,7 @@ function updateTemperature(response) {
   tempElement.innerHTML = roundedTemp;
   timeElement.innerHTML = formatDate(date);
 }
+
 function formatDate(date) {
   let days = [
     "Sunday",
